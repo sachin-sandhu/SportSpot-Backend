bf4321f05e475609cec57c428164ea7f0446dc88
PlaySkyHD <nic14markfort@gmail.com>
2024-10-04 16:13:03 +0200

    Add Event Test

---
+++ SportSpot-Test/Event/Listener/PrioEventListener.cs
@@ -0,0 +1,29 @@
+﻿using SportSpot.Events;
+
+namespace SportSpot_Test.Event
+{
+    internal class PrioEventListener : IListener
+    {
+        public List<EventPriority> PrioritiesCallStack { get; private set; } = [];
+
+        [EventHandler(EventPriority.LOW)]
+#pragma warning disable IDE0060
+        public void OnTestEventLow(OnTestEvent @event)
+        {
+            PrioritiesCallStack.Add(EventPriority.LOW);
+        }
+
+        [EventHandler(EventPriority.HIGH)]
+        public void OnTestEventHigh(OnTestEvent @event)
+        {
+            PrioritiesCallStack.Add(EventPriority.HIGH);
+        }
+
+        [EventHandler(EventPriority.MEDIUM)]
+        public void OnTestEventMEDIUM(OnTestEvent @event)
+        {
+            PrioritiesCallStack.Add(EventPriority.MEDIUM);
+        }
+#pragma warning restore IDE0060
+    }
+}
