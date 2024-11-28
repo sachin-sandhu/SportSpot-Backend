b7e39b68e082e819ca4e2fd4ddbd416578e218e5
PlaySkyHD <nic14markfort@gmail.com>
2024-11-28 13:30:01 +0100

    Ascending Session

--- SportSpot/V1/Session/Dtos/SessionUserSearchQueryDto.cs
+++ SportSpot/V1/Session/Dtos/SessionUserSearchQueryDto.cs
@@ -11,5 +11,7 @@ namespace SportSpot.V1.Session.Dtos
         public int Page { get; init; }
         [DefaultValue(false)]
         public bool WithExpired { get; init; } = false;
+        [DefaultValue(true)]
+        public bool Ascending { get; init; } = true;
     }
 }
